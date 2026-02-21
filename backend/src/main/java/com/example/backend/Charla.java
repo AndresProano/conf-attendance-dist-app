b9afe01844f1453084985d757a4334cb9c09a4c6
Andrés Proaño <srvhubprod@usfq.edu.ec>
2026-02-21 12:02:43 -0500

    Structure

---
+++ backend/src/main/java/com/example/backend/Charla.java
@@ -0,0 +1,28 @@
+package com.example.backend;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "charlas")
+@Data
+public class Charla {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String titulo;
+	
+	@Column(length = 1000)
+	private String descripcion;
+	
+	private String lugar;
+	private LocalDateTime fechaHora;
+	private int cuposTotales;
+	private int cuposDisponibles;
+
+	// Relación con el Expositor (la crearemos después)
+	// @ManyToOne
+	// private Expositor expositor;
+}
